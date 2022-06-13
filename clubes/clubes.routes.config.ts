868ecd9e572dbbd7207dd42f77c1e126aa5dcae9
Carla Serrano <carlacarly8@gmail.com>
2022-06-13 20:57:52 -0300

    Feat: Configure routes with middleware added

--- clubes/clubes.routes.config.ts
+++ clubes/clubes.routes.config.ts
@@ -7,6 +7,39 @@ export class ClubesRoutes extends CommonRoutesConfig {
   }
 
   configureRoutes() {
+    this.app
+      .route("/clubes")
+      .get((req: express.Request, res: express.Response) => {
+        res.status(200).send(`List of clubes`);
+      });
+
+    this.app
+      .route(`/clubes/:clubId`)
+      .all(
+        (
+          req: express.Request,
+          res: express.Response,
+          next: express.NextFunction
+        ) => {
+          // this middleware function runs before any request to /clubes/:clubId
+          // but it doesn't accomplish anything just yet---
+          // it simply passes control to the next applicable function below using next()
+          next();
+        }
+      )
+      .get((req: express.Request, res: express.Response) => {
+        res.status(200).send(`GET requested for id ${req.params.clubId}`);
+      })
+      .put((req: express.Request, res: express.Response) => {
+        res.status(200).send(`PUT requested for id ${req.params.clubId}`);
+      })
+      .patch((req: express.Request, res: express.Response) => {
+        res.status(200).send(`PATCH requested for id ${req.params.clubId}`);
+      })
+      .delete((req: express.Request, res: express.Response) => {
+        res.status(200).send(`DELETE requested for id ${req.params.clubId}`);
+      });
+
     return this.app;
   }
 }
