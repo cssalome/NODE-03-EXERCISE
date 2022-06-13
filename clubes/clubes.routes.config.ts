8e002494c6c3457b2edfc80dd65961060126ff0d
Carla Serrano <carlacarly8@gmail.com>
2022-06-13 19:48:48 -0300

    Feat: common routes and clubes routes added

---
+++ clubes/clubes.routes.config.ts
@@ -0,0 +1,8 @@
+import { CommonRoutesConfig } from "../common/common.routes.config";
+import express from "express";
+
+export class ClubesRoutes extends CommonRoutesConfig {
+  constructor(app: express.Application) {
+    super(app, "ClubesRoutes");
+  }
+}
